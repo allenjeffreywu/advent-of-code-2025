9288f20684d40abfe433c024c2bc553f3b010682
Allen Wu <allenwuhoo@gmail.com>
2025-12-02 15:51:19 -0800

    day2 p2 Done

--- day2.py
+++ day2.py
@@ -16,8 +16,8 @@ def processRanges(fileName: str) -> list:
     return result
 
 
-def findInvalidId(ranges: list) -> int:
-    """findInvalidId takes in a list of ranges and returns the sum of all invalid ids
+def findInvalidIdP1(ranges: list) -> int:
+    """findInvalidIdP1 takes in a list of ranges and returns the sum of all invalid ids that have 2 repeated sequences
 
     Args:
         ranges (list): list of ranges. Ranges are formatted [begin, end]
@@ -46,14 +46,57 @@ def findInvalidId(ranges: list) -> int:
     return sumIds
 
 
+def checkInvalidP2(idValue: int) -> bool:
+    numDigits = int(math.log10(idValue) + 1)
+    idStr = str(idValue)
+    for i in range(2, numDigits + 1):
+        segmentLength = numDigits / i
+        if not segmentLength.is_integer():
+            continue
+        segmentLength = int(segmentLength)
+        j = 0
+        while j < numDigits - segmentLength:
+            if idStr[j:j + segmentLength] != idStr[j + segmentLength: j + (2 * segmentLength)]:
+                break
+            # print(
+            #     f"first: {idStr[j:j + segmentLength]} second: {idStr[j + segmentLength: j + (2 * segmentLength)]}")
+            j += segmentLength
+        if j == numDigits - segmentLength:
+            return True
+    return False
+
+
+def findInvalidIdP2(ranges: list) -> int:
+    """findInvalidIdP1 takes in a list of ranges and returns the sum of all invalid ids that have any number of repeated sequences
+
+    Args:
+        ranges (list): list of ranges. Ranges are formatted [begin, end]
+
+    Returns:
+        int: sum of all invalid ids
+    """
+    sumIds = 0
+    for b, e in ranges:
+        for i in range(b, e + 1):
+            if checkInvalidP2(i):
+                sumIds += i
+
+    return sumIds
+
+
 class TestFindInvalidId(unittest.TestCase):
-    def testFindInvalidId(self):
+    def testFindInvalidIdP1(self):
         rangeList = processRanges("input/day2test.txt")
-        self.assertEqual(findInvalidId(rangeList), 1227775554)
+        self.assertEqual(findInvalidIdP1(rangeList), 1227775554)
+
+    def testFindINvalidIdP2(self):
+        rangeList = processRanges("input/day2test.txt")
+        self.assertEqual(findInvalidIdP2(rangeList), 4174379265)
 
 
 if __name__ == "__main__":
     # unittest.main()
-    # actual work for day2 p1
+
+    # actual work for day2 p2
     rangeList = processRanges("input/day2.txt")
-    print(findInvalidId(rangeList))
+    print(findInvalidIdP2(rangeList))
