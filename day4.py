933d7f529d117b03a1ef3c0feb70babb3d2f595c
Allen Wu <allenwuhoo@gmail.com>
2025-12-04 15:29:25 -0800

    day4 cleanup

--- day4.py
+++ day4.py
@@ -1,12 +1,6 @@
 import unittest
 
 
-# we can brute force this one, for each cell check all 8 directions. Probably not ideal but it will work
-# we can minimize checking by having a sliding window. we ignore the roll at the selected index
-# just check all around it
-
-# nah let's just check all around with some bounds checking
-
 def readFloor(fileName: str) -> list[list[str]]:
     floor = []
     with open(fileName, "r") as file:
@@ -26,16 +20,12 @@ def checkBoundary(r: int, c: int, floor: list[list[str]]) -> bool:
                   [-1, 0], [-1, -1], [0, -1], [1, -1]]
 
     if r == 0:
-        # remove all directions that have -1 for r
         directions = list(filter(lambda x: x[0] != -1, directions))
     if c == 0:
-        # remove all directions that have -1 for c
         directions = list(filter(lambda x: x[1] != -1, directions))
     if r == len(floor) - 1:
-        # remove all directions that have 1 for r
         directions = list(filter(lambda x: x[0] != 1, directions))
     if c == len(floor[0]) - 1:
-        # remove all directions that have 1 for c
         directions = list(filter(lambda x: x[1] != 1, directions))
     count = 0
     for dr, dc in directions:
@@ -43,19 +33,7 @@ def checkBoundary(r: int, c: int, floor: list[list[str]]) -> bool:
             count += 1
         if count >= MAX_ADJACENT_ROLLS:
             return False
-
     return True
-
-
-def rollsAccessibleByForkliftP1(floor: list[list[str]]) -> int:
-    ROWS = len(floor)
-    COLS = len(floor[0])
-    totalRollsAccessibleByForklift = 0
-    for r in range(ROWS):
-        for c in range(COLS):
-            if floor[r][c] == "@" and checkBoundary(r, c, floor):
-                totalRollsAccessibleByForklift += 1
-    return totalRollsAccessibleByForklift
 
 
 def getRemovableRollLocations(floor: list[list[str]]) -> list[list[int]]:
@@ -69,16 +47,19 @@ def getRemovableRollLocations(floor: list[list[str]]) -> list[list[int]]:
     return rollLocations
 
 
+def getNumberOfAccessibleRolls(floor: list[list[str]]) -> int:
+    return len(getRemovableRollLocations(floor))
+
+
 def removeRolls(floor: list[list[str]], rollLocations=list[list[int]]) -> None:
     for r, c in rollLocations:
         floor[r][c] = "."
 
 
-def getMaxNumberOfRollsThatCanBeRemoved(floor: list[list[str]]) -> int:
+def getNumberOfRollsThatCanBeRemoved(floor: list[list[str]]) -> int:
     ROWS = len(floor)
     COLS = len(floor[0])
     totalRollsRemoved = 0
-    # must calculate first pass, then we can while loop for subsequent steps
 
     rollLocations = getRemovableRollLocations(floor)
     while len(rollLocations) > 0:
@@ -89,13 +70,13 @@ def getMaxNumberOfRollsThatCanBeRemoved(floor: list[list[str]]) -> int:
 
 
 class TestRollsAccessibleByForklift(unittest.TestCase):
-    def testRollsAccessibleByForkliftP1(self):
+    def testGetNumberOfAccessibleRolls(self):
         floor = readFloor("input/day4test.txt")
-        self.assertEqual(rollsAccessibleByForkliftP1(floor), 13)
+        self.assertEqual(getNumberOfAccessibleRolls(floor), 13)
 
-    def testGetMaxNumberOfRollsThatCanBeRemoved(self):
+    def testGetNumberOfRollsThatCanBeRemoved(self):
         floor = readFloor("input/day4test.txt")
-        self.assertEqual(getMaxNumberOfRollsThatCanBeRemoved(floor), 43)
+        self.assertEqual(getNumberOfRollsThatCanBeRemoved(floor), 43)
 
 
 if __name__ == "__main__":
