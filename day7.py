b26466e483d49c32acda5bb9e8ad0f0d767e6fcf
Allen Wu <allenwuhoo@gmail.com>
2025-12-08 17:58:59 -0800

    day7 p2 Done

--- day7.py
+++ day7.py
@@ -1,5 +1,5 @@
 import unittest
-from collections import deque
+from collections import deque, defaultdict
 
 
 def readManifold(fileName: str) -> tuple[list[list[str]], list[int]]:
@@ -17,20 +17,20 @@ def readManifold(fileName: str) -> tuple[list[list[str]], list[int]]:
     return manifold, start
 
 
-def countBeamSplits(manifold: list[list[str]], start: list[int]) -> int:
+def getBeamSplits(manifold: list[list[str]], start: list[int]) -> list[list[int]]:
     ROWS = len(manifold)
     COLS = len(manifold[0])
-    numberOfSplits = 0
+    splitLocations = []
     queue = deque()
     visited = set()
     queue.append([start[0] + 1, start[1]])
     while queue:
-        r, c = queue.pop()
+        r, c = queue.popleft()
         if (r, c) in visited:
             continue
         visited.add((r, c))
         if manifold[r][c] == "^":
-            numberOfSplits += 1
+            splitLocations.append([r, c])
             if c + 1 < COLS and r < ROWS - 1:
                 queue.append([r + 1, c + 1])
             if c - 1 >= 0 and r < ROWS - 1:
@@ -38,21 +38,49 @@ def countBeamSplits(manifold: list[list[str]], start: list[int]) -> int:
         elif r < ROWS - 1:
             queue.append([r + 1, c])
 
-    return numberOfSplits
+    return splitLocations
 
 
-def countTimeLines(manifold: list[list[str]], start: list[int]) -> int:
-    return -1
+def countTimeLines(manifold: list[list[str]], start: list[int], splitLocations: list[list[int]]) -> int:
+    locations = splitLocations.copy()
+    locations.append(start)
+    locations.sort(reverse=True)
+    queue = deque(locations)
+    # intuition is that each split means 2 timelines
+    timeLines = defaultdict(lambda: 2)
+    while queue:
+        r, c = queue.popleft()
+
+        if r == len(manifold) - 2:
+            timeLines[(r, c)] = 2
+
+        for nr, nc in queue:
+            if nc == c:
+                break
+            if nc == c - 1 or nc == c + 1:
+                # intuition is that we are only adding to one side, we have not calculated the other
+                # if this happens a second time, then the original count of 2 timelines is completely
+                # replaced by our calculations.
+                timeLines[(nr, nc)] += timeLines[(r, c)] - 1
+
+    return timeLines[(start[0], start[1])]
 
 
 class TestManifold(unittest.TestCase):
     def testCountBeamSplits(self):
         manifold, start = readManifold("input/day7test.txt")
-        self.assertEqual(countBeamSplits(manifold, start), 21)
+        self.assertEqual(len(getBeamSplits(manifold, start)), 21)
+
+    def testCountTimeLines(self):
+        manifold, start = readManifold("input/day7test.txt")
+        splitLocations = getBeamSplits(manifold, start)
+        self.assertEqual(countTimeLines(manifold, start, splitLocations), 40)
 
 
 if __name__ == "__main__":
     # unittest.main()
 
     manifold, start = readManifold("input/day7.txt")
-    print("Answer: ", countBeamSplits(manifold, start))
+    splitLocations = getBeamSplits(manifold, start)
+
+    print("Answer: ", countTimeLines(manifold, start, splitLocations))
